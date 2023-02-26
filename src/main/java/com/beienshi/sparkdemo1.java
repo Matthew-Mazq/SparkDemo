2c188b851d6e8cdf1faef994e62586d2e41aac32
mazhiqiu <mazhiqiu@foxmail.com>
2023-02-26 19:30:29 +0800

    second commit

--- src/main/java/com/beienshi/sparkdemo1.java
+++ src/main/java/com/beienshi/sparkdemo1.java
@@ -3,5 +3,6 @@ package com.beienshi;
 public class sparkdemo1 {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,spark");
     }
 }
